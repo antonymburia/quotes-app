80141c5c19ef4ba13d7c8d087f9fd102d2b22cef
antonymburia <antonymburia10@gmail.com>
2022-02-06 20:36:25 +0300

    create like and dislike functionality

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -9,15 +9,26 @@ export class BodyComponent implements OnInit {
   user = 'Antony Mburia'
   authorname = 'Benjamin Franklin'
   content = 'Tell me and I forget. Teach me and I remember. Involve me and I learn. -'
+  day: Date = new Date()
+  likes: number = 0
+  unlikes: number = 0
 
   quote = [
-    { name: this.user, authorname: this.authorname, content: this.content }
+    { name: this.user, authorname: this.authorname, content: this.content, likes: 0, unlikes: 0, day: new Date(), }
   ]
+  addlike(){
+    this.likes++;
+    return this.likes;
+  }
+  dislike(){
+    this.unlikes++;
+  }
   addquote() {
     this.quote.push(
-      { name: this.user, authorname: this.authorname, content: this.content }
+      { name: this.user, authorname: this.authorname, content: this.content, likes:this.likes, unlikes:this.unlikes, day: this.day}
     )
   }
+  
 
   constructor() { }
 
