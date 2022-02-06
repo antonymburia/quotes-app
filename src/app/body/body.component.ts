5a18775e214f56c0b08c80b7839db66c2c5dd49e
antonymburia <antonymburia10@gmail.com>
2022-02-06 22:33:32 +0300

    modify like and dislike functions

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -13,16 +13,34 @@ export class BodyComponent implements OnInit {
   day: Date = new Date()
   likes: number = 0
   unlikes: number = 0
+  id: number = 0;
 
   quotes: Quote[] = [
-    new Quote(1, "Benjaminfranklin", "Antony Mburia", "Tell me and I forget. Teach me and I remember. Involve me and I learn.", new Date(), 0, 0)
+    new Quote(1, "Benjaminfranklin", "Antony Mburia", "Tell me and I forget. Teach me and I remember. Involve me and I learn.", new Date())
   ]
-  addNewQuote(quote) {
-    quote.author = quote.author;
-    quote.user = quote.user;
-    quote.content = quote.content;
-    this.quotes.push(quote)
+  
+
+  // addNewQuote(quote: Quote) {
+  //   quote.authorname = quote.authorname;
+  //   quote.user = quote.user;
+  //   quote.content = quote.content;
+  //   this.quotes.push(quote)
+  // }
+addlike(index:number){
+  this.quotes[index].likes++;
+}
+dislike(index:number){
+  this.quotes[index].unlikes++
+}
+  
+
+  addquote() {
+    this.quotes.push(
+      { id: this.id, authorname: this.authorname, user: this.user, content: this.content, likes: this.likes, unlikes: this.unlikes, day: this.day }
+    )
   }
+
+
   constructor() { }
 
   ngOnInit(): void {
