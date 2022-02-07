60d297ccb9a33e2bbafc7937f05ccf85696a98d8
antonymburia <antonymburia10@gmail.com>
2022-02-07 23:41:24 +0300

    create best quote filter

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -15,12 +15,12 @@ export class BodyComponent implements OnInit {
   likes: number = 0
   unlikes: number = 0
   id: number = 0;
-  time:number = 0;
-  key:string = ''
+  time: number = 0;
+  key: string = ''
 
   quotes: Quote[] = [
-    new Quote(0, 'Mark Twain', 'Antony Mburia', '“Clothes make the man. Naked people have little or no influence in society.”', new Date(2022, 2, 0, 20, 0, 17)),
-    new Quote(1, 'Benjamin Franklin', 'Antony Mburia', 'Tell me and I forget. Teach me and I remember. Involve me and I learn. -', new Date(2022, 2, 2, 15, 58, 17))
+    new Quote(0, 'Mark Twain', 'Antony Mburia', '“Clothes make the man. Naked people have little or no influence in society.”', new Date(2022, 2, 2, 15, 58, 17)),
+    new Quote(1, 'Benjamin Franklin', 'Antony Mburia', 'Tell me and I forget. Teach me and I remember. Involve me and I learn. -', new Date(2022, 2, 0, 20, 0, 17))
 
   ]
 
@@ -40,12 +40,12 @@ export class BodyComponent implements OnInit {
     }
 
     this.quotes.push(
-      { id: this.id, authorname: this.authorname, user: this.user, content: this.content, likes: this.likes, unlikes: this.unlikes, day: this.day, time:this.time, key:this.key}
+      { id: this.id, authorname: this.authorname, user: this.user, content: this.content, likes: this.likes, unlikes: this.unlikes, day: this.day, time: this.time, key: this.key }
     );
     this.authorname = '';
     this.user = '';
     this.content = '';
-    
+
   }
 
 
@@ -56,38 +56,54 @@ export class BodyComponent implements OnInit {
 
 
 
-  timediff(index:number) {
+  timediff(index: number) {
     var uploadtime = new Date(this.quotes[index].day).getTime()
     var newtime = new Date().getTime()
     var diff = uploadtime - newtime;
     var dayspassed = Math.floor(diff / 86400000);//days
     var timehours = Math.floor((diff % 86400000) / 3600000); //hours
     var timeminutes = Math.ceil(((diff % 86400000) / 3600000) / 60000); //minutes
-  
-    if (dayspassed >=1 ) {
+
+    if (dayspassed >= 1) {
       this.quotes[index].time = dayspassed
       return this.quotes[index].time;
     } else {
       this.quotes[index].time = timehours
-      return  this.quotes[index].time = timehours 
-      
+      return this.quotes[index].time = timehours
+
     }
   }
-  timehours(i:any){
+  timehours(i: any) {
     var uploadtime = new Date(this.quotes[i].day).getTime()
     var newtime = new Date().getTime()
     var diff = newtime - uploadtime;
     var dayspassed = Math.floor(diff / 86400000);//days
     var timehours = Math.floor((diff % 86400000) / 3600000); //hours
     var timeminutes = Math.ceil(((diff % 86400000) / 3600000) / 60000); //minutes
-    
-    if (dayspassed >=1) {
-      return  i = "days ago"
+
+    if (dayspassed >= 1) {
+      return i = "days ago"
     } else {
-      return i ="hours ago"
-     
+      return i = "hours ago"
+
     }
-  
+
+  }
+
+  best!: number;
+
+  bestquote() {
+    this.best = 0;
+
+    for (let i = 0; i < this.quotes.length; i++) {
+      if (this.quotes[i].likes > this.best) {
+        this.content = this.quotes[i].content;
+        this.authorname = this.quotes[i].authorname;
+        this.user = this.quotes[i].user;
+        this.best = this.quotes[i].likes;
+      }
+
+    }
   }
 
 
