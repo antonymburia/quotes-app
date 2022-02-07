0d0bce69ad307159405275b10c77bbada1d24835
antonymburia <antonymburia10@gmail.com>
2022-02-07 17:25:28 +0300

    modify body

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quote';
 
@@ -7,17 +8,17 @@ import { Quote } from '../quote';
   styleUrls: ['./body.component.css']
 })
 export class BodyComponent implements OnInit {
-  user = 'Antony Mburia'
-  authorname = 'Benjamin Franklin'
-  content = 'Tell me and I forget. Teach me and I remember. Involve me and I learn. -'
-  day: Date = new Date()
+  user = ''
+  authorname = ''
+  content = ''
+  day: Date = new Date(2022, 2, 1, 12, 58, 17)
   likes: number = 0
   unlikes: number = 0
   id: number = 0;
 
   quotes: Quote[] = [
-    new Quote(0, 'Mark Twain', this.user, '“Clothes make the man. Naked people have little or no influence in society.”', this.day),
-    new Quote(1, this.authorname, this.user, this.content, this.day)
+    new Quote(0, 'Mark Twain', 'Antony Mburia', '“Clothes make the man. Naked people have little or no influence in society.”', this.day),
+    new Quote(1, 'Benjamin Franklin', 'Antony Mburia', 'Tell me and I forget. Teach me and I remember. Involve me and I learn. -', this.day)
 
   ]
 
@@ -32,28 +33,34 @@ export class BodyComponent implements OnInit {
 
 
   addquote() {
-    if (this.authorname=='') {
+    if (this.authorname == '') {
       this.authorname = 'unknown';
-      
-    } 
+
+    }
 
     this.quotes.push(
       { id: this.id, authorname: this.authorname, user: this.user, content: this.content, likes: this.likes, unlikes: this.unlikes, day: this.day }
     );
-    this.authorname ='';
+    this.authorname = '';
     this.user = '';
-    this. content ='';
+    this.content = '';
   }
 
-  delete(index:number){
+  delete(index: number) {
     this.quotes.splice(index, 1)
   }
 
 
+  uploadtime = new Date(this.day).getTime()
+  newtime = new Date().getTime()
+  timepassed = this.newtime - this.uploadtime;
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+
+
+constructor() { }
+
+ngOnInit(): void {
+}
 
 }
