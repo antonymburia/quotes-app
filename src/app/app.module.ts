752791ecaeaba09dd0a292377006b0aee3128c32
antonymburia <antonymburia10@gmail.com>
2022-02-10 12:26:39 +0300

    add custom directive to highlight best quote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
 import { FormsModule } from '@angular/forms';
+import { HighlightDirective } from './highlight.directive';
 
 
 
@@ -15,7 +16,8 @@ import { FormsModule } from '@angular/forms';
     AppComponent,
     NavigationComponent,
     FooterComponent,
-    BodyComponent
+    BodyComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
